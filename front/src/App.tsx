3db2ef3c1ba2d6fad5ca918e30168ec6b53b1ecf
toshiyuki <toshiyuki.saito.16@gmail.com>
2020-04-03 09:58:17 +0900

    #05

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
-import { Container, CssBaseline } from "@material-ui/core";
+import { Container, CssBaseline, Grid, makeStyles } from "@material-ui/core";
+import red from "@material-ui/core/colors/red";
 
 interface Props {}
 
-const App: React.FC<Props> = props => {
-  const [count, setCount] = useState<number>(0);
+const useStyles = makeStyles(theme => ({
+  red: {
+    backgroundColor: red[500]
+  }
+}));
 
+const App: React.FC<Props> = props => {
+  const classes = useStyles();
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="xl">
       <CssBaseline />
-      hello!
+      <Grid container spacing={3}>
+        <Grid item xs={3} className={classes.red}>
+          bus
+        </Grid>
+        <Grid item xs={6}>
+          main
+        </Grid>
+        <Grid item xs={3}>
+          users
+        </Grid>
+      </Grid>
     </Container>
   );
 };
